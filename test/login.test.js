5509fa3befa02f20afb10c5caef02f2218f60338
Jeff <jeffersonc.silva@hotmail.com>
2025-07-30 22:30:38 -0300

    first project

---
+++ test/login.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+describe('Login', () => {
+  describe('POST /login', () => {
+    it('should return 200 for token as string', async () => {
+      const response = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({ username: 'julio.lima', senha: '123456' })
+
+      expect(response.status).to.equal(200)
+      expect(response.body.token).to.be.a('string')
+    })
+  })
+})
