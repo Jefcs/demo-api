7d5ca9b12ea60eef0091c730d472a4c0399d487e
Jeff <jeffersonc.silva@hotmail.com>
2025-07-31 23:40:16 -0300

    finish refactor

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -1,10 +1,13 @@
 const request = require('supertest')
+const postLogin = require('../fixtures/postLogin.json')
 
 const getToken = async (username, password) => {
+  const bodyLogin = { ...postLogin }
+
   const response = await request(process.env.BASE_URL)
     .post('/login')
     .set('Content-Type', 'application/json')
-    .send({ username, senha: password })
+    .send(bodyLogin)
 
   return response.body.token
 }
--- test/login.test.js
+++ test/login.test.js
@@ -1,14 +1,18 @@
 const request = require('supertest')
 const { expect } = require('chai')
+const postLogin = require('../fixtures/postLogin.json')
+
 require('dotenv').config()
 
 describe('Login', () => {
   describe('POST /login', () => {
     it('should return 200 for token as string', async () => {
+      const bodyLogin = { ...postLogin }
+
       const response = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({ username: 'julio.lima', senha: '123456' })
+        .send(bodyLogin)
 
       expect(response.status).to.equal(200)
       expect(response.body.token).to.be.a('string')
