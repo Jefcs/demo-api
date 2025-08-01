ae1a0a58ab0869dbf3c0e6b8cda2f97d5fad949e
Jeff <jeffersonc.silva@hotmail.com>
2025-07-31 22:46:06 -0300

    added test for transfering amounts

---
+++ test/transfers.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+describe('Transfers', () => {
+  describe('POST /transferencias', () => {
+    it('must return 201 when the amount transferred is equal or greater than R$10,00', async () => {
+      const responseLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({ username: 'julio.lima', senha: '123456' })
+
+      const token = responseLogin.body.token
+
+      const response = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ contaOrigem: 1, contaDestino: 2, valor: 150, token: '' })
+
+      expect(response.status).to.equal(201)
+    })
+
+    it('must return 422 when the amount transferred is less than R$10,00', async () => {
+      const responseLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({ username: 'julio.lima', senha: '123456' })
+
+      const token = responseLogin.body.token
+
+      const response = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ contaOrigem: 1, contaDestino: 2, valor: 8, token: '' })
+
+      expect(response.status).to.equal(422)
+    })
+  })
+})
